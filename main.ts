945e7dba90ea4e6fb1f813b7b44b304aa453bdee
Juan Arboleda <j.arboleda@uninades.edu.co>
2022-04-19 19:11:20 -0500

    Parte 1 V1

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import { Serie } from "./serie.js";
 import { dataSeries } from "./dataSeries.js";
 let seriesTBody : HTMLElement = document.getElementById("series")!;
+let promTemporadas : HTMLElement = document.getElementById("promTemporadas")!;
 
 renderSeriesInTable(dataSeries);
+
+promTemporadas.innerHTML = calcularPromedioTemporadas(dataSeries).toString();
 
 function renderSeriesInTable(series : Serie[]) : void {
     console.log("Desplegando series");
@@ -14,4 +17,14 @@ function renderSeriesInTable(series : Serie[]) : void {
                                 <td>${serie.seasons}</td>`;
         seriesTBody.appendChild(trElement);
     })
+}
+
+function calcularPromedioTemporadas(series: Serie[]) : number {
+    console.log("Calculando promedio de temporadas")
+    let suma: number = 0;
+    series.forEach((serie) => {
+        suma += serie.seasons;
+    });
+    let promedio : number = suma / (series.length);
+    return promedio;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import { dataSeries } from "./dataSeries.js";
 var seriesTBody = document.getElementById("series");
+var promTemporadas = document.getElementById("promTemporadas");
 renderSeriesInTable(dataSeries);
+promTemporadas.innerHTML = calcularPromedioTemporadas(dataSeries).toString();
 function renderSeriesInTable(series) {
     console.log("Desplegando series");
     series.forEach(function (serie) {
@@ -8,4 +10,13 @@ function renderSeriesInTable(series) {
         trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                                <td>").concat(serie.name, "</td>\n                                <td>").concat(serie.channel, "</td>\n                                <td>").concat(serie.seasons, "</td>");
         seriesTBody.appendChild(trElement);
     });
+}
+function calcularPromedioTemporadas(series) {
+    console.log("Calculando promedio de temporadas");
+    var suma = 0;
+    series.forEach(function (serie) {
+        suma += serie.seasons;
+    });
+    var promedio = suma / (series.length);
+    return promedio;
 }
